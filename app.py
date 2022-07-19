ccdd60a93499f323475e2b139282794807a1e0aa
Qlwentt <quai.wentt@gmail.com>
2022-07-19 18:06:20 -0400

    fix CORS issue

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
 import json
 from flask import Flask, Response, jsonify, request
-from constants.constants import VALID_POSITIONS
+from flask_cors import CORS, cross_origin
 
+from constants.constants import VALID_POSITIONS
 from enums.moves import Moves
 from src.board import Board
 from src.computer import Computer
 
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
+
 
 @app.route("/")
 def hello_world():
     return jsonify("Hello, World!")
 
 @app.post('/api/get-move')
+@cross_origin(supports_credentials=True)
 def getMove():
     req = request.get_json()
-    print(req)
     if any(req.get(key,None) is None for key in ['board', 'player', 'difficulty']):
         response = json.dumps({'error': 'Missing required parameters'})
-        print(req, type(req))
         return Response(response, status=422, mimetype='application/json')
-    print('it got here')
     flatBoard = sum(req['board'],[])
-    flatBoard = [None if x == "None" else x for x in flatBoard]
+    flatBoard = [None if x == "None" or x == "" else x for x in flatBoard]
     board = Board.getBoardObj(flatBoard)
     player = req['player']
     opponent = Moves.X.value if player == Moves.O.value else Moves.O.value
